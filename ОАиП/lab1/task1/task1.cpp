8b075228c09718cd2cb1ecba4e20eb1aff39f925
Bernathh <bernat6545@gmail.com>
2026-09-17 21:09:01 +0300

    обновил лабу 1 под экз

--- ОАиП/lab1/task1/task1.cpp
+++ ОАиП/lab1/task1/task1.cpp
@@ -5,11 +5,12 @@
 using namespace std;
 
 int main() {
-    double x, y, z;
 
     //       Вариант 4
     //   x      y       z
     // 0.4e4 -0.875 -0.475e-3
+    
+    double x = 0.0, y = 0.0, z = 0.0;
 
     cout << "Enter x, y, z: ";
     cin >> x >> y >> z;
@@ -19,20 +20,25 @@ int main() {
         return 1;
     }
 
-     double base = fabs(cos(x) - cos(y));
-     double exponent = 1 + 2 * pow(sin(y), 2);
+    double base = fabs(cos(x) - cos(y));
+    double exponent = 1.0 + 2.0 * pow(sin(y), 2);
 
-     if (base == 0 && exponent <= 0) {
+    if (base == 0.0 && exponent <= 0.0) {
         cerr << "Error: Math error!\n";
         return 2;
-     }
+    }
 
-     double part1 = pow(base, exponent);
-     double part2 = 1 + z + pow(z, 2) / 2.0 + pow(z, 3) / 3.0 + pow(z, 4) / 4.0;
+    double part1 = pow(base, exponent);
 
-     double result = part1 * part2;
+    double z2 = z * z;
+    double z3 = z2 * z;
+    double z4 = z3 * z;
+    double part2 = 1.0 + z + (z2 / 2.0) + (z3 / 3.0) + (z4 / 4.0);
 
-     cout << fixed << setprecision(4);
-     cout << "Result = " << result << '\n';
-     return 0;
- }
+    double result = part1 * part2;
+
+    cout << fixed << setprecision(4);
+    cout << "Result = " << result << '\n';
+
+    return 0;
+}
